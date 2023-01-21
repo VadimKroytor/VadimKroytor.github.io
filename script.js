68e187eae4f849695eb7c7490c927c63492df4da
Sage Calanthe <vadimkroytor@gmail.com>
2023-01-21 12:38:14 -0500

    added fadein animation

---
+++ script.js
@@ -0,0 +1,13 @@
+const items = document.querySelectorAll('.animation');
+
+const active = function(entries){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+        entry.target.classList.add('fadeIn'); 
+        }
+    });
+}
+const io2 = new IntersectionObserver(active);
+ for(let i=0; i < items.length; i++){
+    io2.observe(items[i]);
+ }
